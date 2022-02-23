f2fcc2451d117468a50ba46918845e455be7a367
Krfld <rodrigofldias@gmail.com>
2022-02-23 00:29:47 +0000

    .

--- AlarmClock_ESP-01/include/main.h
+++ AlarmClock_ESP-01/include/main.h
@@ -8,7 +8,27 @@
 #define WIFI_SSID "MEO-8BF0E0"
 #define WIFI_PASSWORD "1ad75b8b11"
 
-#define API_KEY "API_KEY"
+// #define API_KEY "API_KEY"
 
-#define DATABASE_URL "krfld-projects-default-rtdb.europe-west1.firebasedatabase.app"
-#define DATABASE_SECRET "gKMRB9kHbXm7Rdq3YjYkVTMtgbx5kmSaTdyGGuhx"
+// #define DATABASE_URL "krfld-projects-default-rtdb.europe-west1.firebasedatabase.app"
+// #define DATABASE_SECRET "gKMRB9kHbXm7Rdq3YjYkVTMtgbx5kmSaTdyGGuhx"
+
+#define VIBRATOR_PIN 2
+
+typedef struct
+{
+	bool state;
+	bool active;
+	float time;
+	int repeat;
+} Alarm;
+
+/*{
+'state': false, // bool
+'active': true, // bool
+'time': h.m, // float
+'repeat': 'daily', // once - 0 | daily - 1 | onlyWeek - 2 | onlyWeekend - 3
+}*/
+
+void sync();
+void play();
--- AlarmClock_ESP-01/src/main.cpp
+++ AlarmClock_ESP-01/src/main.cpp
@@ -1,26 +1,59 @@
 #include "main.h"
 
+// TODO search sleep mode
+
 void setup()
 {
-  Serial.begin(BAUDRATE);
+	pinMode(VIBRATOR_PIN, OUTPUT);
 
-  Serial.print("Connecting...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
+	Serial.begin(BAUDRATE);
 
-  Serial.print("WiFi connected to ");
-  Serial.println(WiFi.localIP());
+	Serial.print("Connecting...");
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.print(".");
+		delay(500);
+	}
+	Serial.println();
 
-  // Firebase.begin(DATABASE_URL, DATABASE_SECRET);
+	Serial.print("WiFi connected to ");
+	Serial.println(WiFi.localIP());
+
+	// Firebase.begin(DATABASE_URL, DATABASE_SECRET);
 }
+
+Alarm alarm;
 
 void loop()
 {
-  Serial.println("Running...");
-  delay(1000);
+	// Serial.println("Running...");
+	// delay(1000);
+
+	if (alarm.state)
+		play();
+	// Check alarm off on cloud
+	// Check repeat
+	else
+	{
+		sync();
+		// Check alarm on locally (if not ignore and repeat)
+		// if (alarm.state) set true on cloud
+	}
+}
+
+void play()
+{
+	Serial.println("On");
+	digitalWrite(VIBRATOR_PIN, HIGH);
+	delay(1000);
+
+	Serial.println("Off");
+	digitalWrite(VIBRATOR_PIN, LOW);
+	delay(1000);
+}
+
+void sync()
+{
+	// Sync every 3 hours
 }
